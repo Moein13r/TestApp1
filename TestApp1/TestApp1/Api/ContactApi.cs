c7930f3d49679afefd160b4d3d0a5dceeab4c85d
Moein13r <mrahmat1386@gmail.com>
2022-05-15 10:03:13 +0430

    Edited Api Method

--- TestApp1/TestApp1/Api/ContactApi.cs
+++ TestApp1/TestApp1/Api/ContactApi.cs
@@ -11,20 +11,34 @@ namespace TestApp1.Api
     public class ContactApi
     {
         private Base Base = new Base();
-        private string Url = "";
+        private string Url = "https://localhost:7020/api/ContactsApi/";
         public async Task<List<Contact>> GetContactsByName(string name,CancellationToken tokenSource)
         {
             try
             {
-                string uri = Url + "";
+                string uri = Url + $"GetContactsByname?name={name}";
+                string JsonContent = await Base.Get(uri);
+                if (JsonContent == null)
+                    return null;                    
+                List<Contact> items = DataConverter.JsonConverter<Contact[]>.JsonToObject(JsonContent).ToList();
+                tokenSource.ThrowIfCancellationRequested();
+                return items ?? new List<Contact>();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        public async Task<List<Contact>> GetAllContacts(CancellationToken tokenSource)
+        {
+            try
+            {
+                string uri = Url + $"GetAllContacts";
                 string JsonContent = await Base.Get(uri);
                 if (JsonContent == null)
                     return null;
-                if (tokenSource.IsCancellationRequested)
-                {
-                    tokenSource.ThrowIfCancellationRequested();
-                }
                 List<Contact> items = DataConverter.JsonConverter<Contact[]>.JsonToObject(JsonContent).ToList();
+                tokenSource.ThrowIfCancellationRequested();
                 return items ?? new List<Contact>();
             }
             catch (Exception e)
